83677da5cb3fa764fbcc0bc5f31a8e159b27218c
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2024-10-02 09:56:45 -0300

    theme_btn_finished

--- activities/act_01/public/scripts/script.js
+++ activities/act_01/public/scripts/script.js
@@ -1,13 +1,16 @@
-// Getting elements
-let theme_btn = document.getElementById("theme_btn");
-let num_input = document.getElementById("number_input");
-let main_btn = document.getElementById("main_btn");
-let result = document.getElementById("result");
+// Getting elements.
+const theme_btn_checkbox = document.getElementById("theme_checkbox");
+const num_input = document.getElementById("number_input");
+const main_btn = document.getElementById("main_btn");
+const result = document.getElementById("result");
 
-// Functions
+// Always uncheck the checkbox for each reload page.
+theme_btn_checkbox.checked = false;
 
-// Add an event of click to the theme button to change the theme
-theme_btn.addEventListener("click", (e) => {
+// Functions.
+
+// Add an event of click to the theme button to change the theme.
+theme_btn_checkbox.addEventListener("click", (e) => {
   document.body.classList.toggle("dark_theme");
 });
 
